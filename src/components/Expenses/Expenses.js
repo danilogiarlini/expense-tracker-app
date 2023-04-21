3bb4099dd27b4916e5f77678037cf442a1e0c5f1
Danilo Giarlini <danilogiarlini@gmail.com>
2023-04-21 19:55:46 +0200

    file organized

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from '../UI/Card';
 
-function Expenses(props) {
+const Expenses = (props) => {
     
-    return <div className='expenses'>
+    return <Card className='expenses'>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -23,7 +24,7 @@ function Expenses(props) {
         title={props.expenses[3].title}
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
-       /></div>
+       /></Card>
 }
 
 export default Expenses;
