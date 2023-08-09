fdf155b62d31bfb26577da1cfce4126fff757df7
Danilo Giarlini <danilogiarlini@gmail.com>
2023-08-09 16:49:43 +0200

    finished project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';  // ./ indica che file App è nella STESSA CARTELLA
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
